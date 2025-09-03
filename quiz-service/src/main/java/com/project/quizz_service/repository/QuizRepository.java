0be4ec269bf12796a211296e8167732543d87304
Kali Rishi K <110583211+kalirishik@users.noreply.github.com>
2025-09-03 22:42:18 +0530

    Delete quiz-service directory

--- quiz-service/src/main/java/com/project/quizz_service/repository/QuizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.quizz_service.repository;
-
-import com.project.quizz_service.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizRepository extends JpaRepository<Quiz,Integer> {
-}
